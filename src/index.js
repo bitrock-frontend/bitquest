7cdf0a4242c183aeeac6db96305f1fc705861856
Salvatore Laisa <moebiusmania@gmail.com>
2018-04-19 11:41:46 +0200

    updated readme, begin test refactor in ava, more refactor on source code

--- src/index.js
+++ src/index.js
@@ -1,79 +1,75 @@
 'use strict';
 
-;( (window) => {
+const types = ['json', 'text', 'response'];
 
-  const defaults = (target, obj) => {
-    for (let prop in obj) target[prop] = target[prop] || obj[prop]
-  }
+const checkTypes = (response) => response && types.indexOf(response) >= 0;
 
-  const getQuery = (queryParams) => {
-    const arr = Object.keys(queryParams).map( (k) => {
-      return k + '=' + encodeURIComponent(queryParams[k])
-    })
-    return '?' + arr.join('&')
-  }
+const getQuery = (queryParams) => {
+  const arr = Object.keys(queryParams).map( (key) => {
+    return `${key}=${encodeURIComponent(queryParams[key])}`
+  })
+  return '?' + arr.join('&')
+}
 
-  const _fetch = (method, url, opts, data, queryParams) => {
-    opts.method = method
-    opts.headers = opts.headers || {}
-    opts.responseAs = (opts.responseAs && ['json', 'text', 'response'].indexOf(opts.responseAs) >= 0) ? opts.responseAs : 'json'
+const _fetch = (method, url, opts, data, queryParams) => {
+  opts.method = method
+  opts.headers = opts.headers || {}
+  opts.responseAs = checkTypes(opts.responseAs) ? opts.responseAs : 'json'
 
-    defaults(opts.headers, {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+  opts.headers = Object.assign({}, opts.headers, {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  });
+
+  queryParams ? url += getQuery(queryParams) : url;
+
+  data ? opts.body = JSON.stringify(data) : opts.body = undefined;
+
+  return fetchival.fetch(url, opts)
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        if(opts.responseAs=="response")
+          return response
+        if (response.status == 204)
+          return null;
+        return response[opts.responseAs]();
+      }
+      const err = new Error(response.statusText)
+      err.response = response
+      throw err
     });
+}
 
-    queryParams ? url += getQuery(queryParams) : url;
+const fetchival = (url, opts = {}) => {
 
-    data ? opts.body = JSON.stringify(data) : delete opts.body;
-
-    return fetchival.fetch(url, opts)
-      .then((response) => {
-        if (response.status >= 200 && response.status < 300) {
-          if(opts.responseAs=="response")
-            return response
-          if (response.status == 204)
-            return null;
-          return response[opts.responseAs]();
-        }
-        const err = new Error(response.statusText)
-        err.response = response
-        throw err
-      });
+  const _ = (u, o = {}) => {
+    // Extend parameters with previous ones
+    const _u = `${url}/${u}`;
+    const _opts = Object.assign({}, o, opts);
+    return fetchival(_u, _opts)
   }
 
-  const fetchival = (url, opts = {}) => {
+  _.get = (queryParams) => _fetch('GET', url, opts, null, queryParams);
 
-    const _ = (u, o = {}) => {
-      // Extend parameters with previous ones
-      const _u = `${url}/${u}`;
-      defaults(o, opts)
-      return fetchival(_u, o)
-    }
+  _.post = (data) => _fetch('POST', url, opts, data);
 
-    _.get = (queryParams) => _fetch('GET', url, opts, null, queryParams);
+  _.put =  (data) => _fetch('PUT', url, opts, data);
 
-    _.post = (data) => _fetch('POST', url, opts, data);
+  _.patch = (data) => _fetch('PATCH', url, opts, data);
 
-    _.put =  (data) => _fetch('PUT', url, opts, data);
+  _.delete = () =>  _fetch('DELETE', url, opts);
 
-    _.patch = (data) => _fetch('PATCH', url, opts, data);
+  return _;
+}
 
-    _.delete = () =>  _fetch('DELETE', url, opts);
+// Expose fetch so that other polyfills can be used
+// Bind fetch to window to avoid TypeError: Illegal invocation
+fetchival.fetch = typeof fetch !== 'undefined' ? fetch.bind(window) : null
 
-    return _;
-  }
-
-  // Expose fetch so that other polyfills can be used
-  // Bind fetch to window to avoid TypeError: Illegal invocation
-  fetchival.fetch = typeof fetch !== 'undefined' ? fetch.bind(window) : null
-
-  // Support CommonJS, AMD & browser
-  if (typeof exports === 'object')
-    module.exports = fetchival
-  else if (typeof define === 'function' && define.amd)
-    define(function() { return fetchival })
-  else
-    window.fetchival = fetchival
-
-})(typeof window != 'undefined' ? window : undefined);
+// Support CommonJS, AMD & browser
+if (typeof exports === 'object')
+  module.exports = fetchival
+else if (typeof define === 'function' && define.amd)
+  define(function() { return fetchival })
+else
+  window.fetchival = fetchival
